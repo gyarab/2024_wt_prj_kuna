fd168848c49ce76ee305aa8f18fe117274a7f397
toumes <tomaskuna1@seznam.cz>
2025-04-25 10:38:17 +0200

    brr brr patapim

--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from main.views import get_homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
